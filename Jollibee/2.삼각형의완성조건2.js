7bdd1653e7fdc8f348ac511be9c59360c38d0462
Juram Lee <juram2@naver.com>
2023-08-29 18:41:50 +0900

    :sparkles: 2주차 문제풀이

--- Jollibee/2.삼각형의완성조건2.js
+++ Jollibee/2.삼각형의완성조건2.js
@@ -11,15 +11,19 @@
 - 1 ≤ sides의 원소 ≤ 1,000 */
 
 function solution(sides) {
-  for (let word of dic) {
-    let count = 0;
-    for (let key of spell) {
-      if (word.includes(key)) {
-        count++;
-      }
-    }
-    if (count === spell.length) return 1;
-  }
+  const maxNum = Math.max(...sides);
+  const minNum = Math.min(...sides);
+  let count = 0;
 
-  return 2;
+  for (let i = maxNum - minNum + 1; i <= maxNum; i++) count++;
+  for (let i = maxNum + 1; i < maxNum + minNum; i++) count++;
+  return count;
 }
+
+/*
+return Math.min(...sides) * 2 - 1
+sides = [a,b] 이고(a>b라고 가정, 이는 sort해주면됩니다.) 
+새로 주어지는 변의 길이를 c라고 했을 때, 
+a가 가장 긴변인 경우 즉 a > c인 경우 b + c > a > c 이므로 a > c > a-b 이기 때문에 c의 정수 갯수는 b-1개입니다. 
+/ c가 가장 긴변인 경우도 이런식으로 하면 되고 a=c 인경우 한가지 이므로 2b-1이 나옵니다.
+*/
