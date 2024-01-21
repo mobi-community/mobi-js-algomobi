bdf1fa1bf400bada7fe66ec4bdfeffa48e71a77d
Jihyeong00 <iolpd42@gmail.com>
2024-01-21 09:39:36 +0900

    [프로그래머스]/두개-뽑아서-더하기/level-1/5분

---
+++ season-2/Zero/week_2/두개-뽑아서-더하기.js
@@ -0,0 +1,21 @@
+/*
+설계
+
+1. 중복된 숫자를 모두 제거합니다.
+2. 각 숫자 인덱스 마다 더하기를 반복하여 answer에 넣습니다.
+3. 중복된 숫자를 모두 제거합니다.
+4. 정렬을 하여 내보냅니다.
+
+*/
+
+function solution(numbers) {
+    var answer = [];
+    numbers.forEach((val, ind) => {
+        for (let i = ind + 1; i < numbers.length; i++) {
+            answer.push(val + numbers[i]);
+        }
+    });
+    return = [...new Set(answer)].sort()
+}
+
+console.log(solution([2, 1, 3, 4, 1]));
